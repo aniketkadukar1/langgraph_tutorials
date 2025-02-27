b27b2d813fb778e44f0601a95b1f805029d45d27
Aniket kadukar <aniketdkadukar@gmail.com>
2025-02-27 14:42:51 +0530

    Create Revisor Chain

--- reflexion_agent_system/chains.py
+++ reflexion_agent_system/chains.py
@@ -2,7 +2,7 @@ from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 import datetime
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
-from schema import AnswerQuestion
+from schema import AnswerQuestion, ReviseAnswer
 from langchain_core.output_parsers.openai_tools import PydanticToolsParser
 from langchain_core.messages import HumanMessage
 
@@ -35,7 +35,22 @@ first_responder_prompt_template = actor_prompt_template.partial(
     first_instruction="Provide a detailed ~250 word answer"
 )
 
-first_responder_chain = first_responder_prompt_template | llm.bind_tools(tools=[AnswerQuestion], tool_choice="AnswerQuestion") | pydantic_parser
+first_responder_chain = first_responder_prompt_template | llm.bind_tools(tools=[ReviseAnswer], tool_choice="ReviseAnswer") | pydantic_parser
+
+# Revisor chain instruction prompt
+revise_instruction = """Revise your previous answer using the new information.
+    - You should use the previous critique to add inportant information to your answer.
+        - You must include numerical citations in your revised answer to ensure it can be verified.
+        - Add a "References" section to the bottom of your answer (which does not count towards the word limit). In form of:
+            - [1] https://example.com
+            - [2] https://example.com
+    - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
+    """
+
+# Revisor chain
+revisor_chain = actor_prompt_template.partial(
+    first_instruction= revise_instruction
+) | llm.bind_tools(tools=[AnswerQuestion], tool_choice="ReviseAnswer") | pydantic_parser
 
 response = first_responder_chain.invoke({"messages": [HumanMessage(content="Write me a blog post on how small businesses can leverage AI to grow")]})
 
